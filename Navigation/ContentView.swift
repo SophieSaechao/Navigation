d5f299bb971e042d72c664cdc22c068ddad18b51
Sophie <sunfloweryam@gmail.com>
2025-07-24 10:11:47 -0700

    added the second view+emoji

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,15 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("This is the root view 🌲")
+                NavigationLink (destination: Text("You've arrived to the Second View!") ) {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/
+                }
+            }
+            
         }
-        .padding()
-    }
-}
+
+     }
+ }
 
 #Preview {
     ContentView()
